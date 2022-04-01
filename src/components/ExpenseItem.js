beb45f030a5b87955061e2eb26f63b22a4ba6e5e
cakraocha <cakraocha@gmail.com>
2022-04-01 22:05:34 +1100

    feat: add css to expenseitem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>Date (March 28th 2022)</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Title (Car Insurance)</h2>
-        <div>Amount ($294.67)</div>
+        <div className="expense-item__price">Amount ($294.67)</div>
       </div>
     </div>
   );
