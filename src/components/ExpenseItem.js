9825bb1a291f8ccadb0cafa4c76ac7c0b27debd4
cakraocha <cakraocha@gmail.com>
2022-04-01 22:01:10 +1100

    feat: text of the expenseitem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+  return (
+    <div>
+      <div>Date (March 28th 2022)</div>
+      <div>
+        <h2>Title (Car Insurance)</h2>
+        <div>Amount ($294.67)</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
