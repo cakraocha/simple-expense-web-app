5288e42b9bc2da0cb661e87b7fabaaa4d2e31c1c
cakraocha <cakraocha@gmail.com>
2022-04-02 23:12:29 +1100

    feat: tidy up date format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) { // props is an object that passed in the App.js
-
+  const day = props.date.toLocaleString('en-AU', { day: '2-digit' });
+  const month = props.date.toLocaleString('en-AU', { month: 'long' });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
