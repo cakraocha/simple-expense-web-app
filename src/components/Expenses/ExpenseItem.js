5bace13afb4ad8a17db319857186d390b24e5ead
cakraocha <cakraocha@gmail.com>
2022-04-03 12:53:49 +1000

    feat: further modularise expenses and ui

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   // props is an object that passed in the App.js
