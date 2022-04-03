b8d2de81025db6a780f748ba6c25512a2791feaa
cakraocha <cakraocha@gmail.com>
2022-04-03 10:50:10 +1000

    feat: modularise date and add css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props) { // props is an object that passed in the App.js
-  const day = props.date.toLocaleString('en-AU', { day: '2-digit' });
-  const month = props.date.toLocaleString('en-AU', { month: 'long' });
-  const year = props.date.getFullYear();
+function ExpenseItem(props) {
+  // props is an object that passed in the App.js
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
